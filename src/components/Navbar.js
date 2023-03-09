0cd269f64595b19fbcc2529066d4153623a2a6fc
anagh534 <anaghkrkkl@gmail.com>
2023-03-09 22:05:57 +0530

    modified navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,29 +7,25 @@ import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
 
 export default function Navbar() {
-  const [click,setClick]=useState(false);
-  function handleClick()
-  {
-      if(click===false)
-      setClick(true);
-      else
-      setClick(false);
-  }
+  const [isMobile, setIsMobile] = useState(false)
   return (
     <div className="navbar">
-    {/* <a href="/"><h1 className='WoW'>WoW</h1></a> */}
-    <NavLink to='/'><img className='logo' src='/svgs/logo.svg' alt='WoW Logo'/> </NavLink>
-    <div>
-      <ul onClick={handleClick} className={click?"nav-items active":"nav-items"}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/Regions">Regions</a></li>
-        <li><a href="/Events">Events</a></li>
-        <li><a href="/Sponsors">Sponsors</a></li>
-        <li><a href="/About">About us</a></li>
-        <li><a href="/ContactUs">Contact us</a></li>
-      </ul>
+      {/* <a href="/"><h1 className='WoW'>WoW</h1></a> */}
+      <NavLink to='/'><img className='logo' src='/svgs/logo.svg' alt='WoW Logo' /> </NavLink>
+      <div>
+        <ul className={isMobile ? "nav-links-mobile" : "nav-links"} >
+          <li><a href="/">Home</a></li>
+          <li><a href="/Regions">Regions</a></li>
+          <li><a href="/Events">Events</a></li>
+          <li><a href="/Sponsors">Sponsors</a></li>
+          <li><a href="/About">About us</a></li>
+          <li><a href="/ContactUs">Contact us</a></li>
+        </ul>
       </div>
-      <Button/>
+      <Button />
+      <button className='mobile-menu-icon'>
+        {isMobile ? (<i class="fas fa-times" onClick={() => setIsMobile(false)}></i>) : (<i class="fas fa-bars" onClick={() => setIsMobile(true)}></i>)}
+      </button>
     </div>
-    )
+  )
 }
